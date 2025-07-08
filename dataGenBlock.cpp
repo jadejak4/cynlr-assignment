85dc412c3e6b6bee5b4492b2400f78ff100ffdf7
Krishnapal Jadeja <krishnapal.jadeja@learner.manipal.edu>
2025-07-08 21:15:45 +0530

    dataGen block done. csv file to be added

--- dataGenBlock.cpp
+++ dataGenBlock.cpp
@@ -28,9 +28,9 @@ void randomNumberGen1()
     }
     while (1)
     {
-        uint8_t dataPt = generateRandomNumber();
+        uint8_t dataPt[2] = { generateRandomNumber(), generateRandomNumber() };
 
-        BOOL success = writeToPipe(hPipe, &dataPt, 1);
+        BOOL success = writeToPipe(hPipe, dataPt, 2);
         if(!success)
         {
             std::cerr << "error writing to the pipe " << std::endl;
@@ -40,28 +40,6 @@ void randomNumberGen1()
     CloseHandle(hPipe);
 }
 
-// try using function templates or pointers
-void randomNumberGen2()
-{
-    HANDLE hPipe = createWriterPipe(internalPipeName);
-
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        std::cerr << "Failed to connect to pipe. Error: " << GetLastError() << "\n";
-        return;
-    }
-    while (1)
-    {
-        uint8_t dataPt = generateRandomNumber();
-
-        BOOL success = writeToPipe(hPipe, &dataPt, 1);
-        if(!success)
-        {
-            std::cerr << "error writing to the pipe " << std::endl;
-        }
-    }
-    DisconnectNamedPipe(hPipe);
-    CloseHandle(hPipe);
-}
 
 void sendDataToFilterBlock()
 {
@@ -80,11 +58,7 @@ void sendDataToFilterBlock()
             BOOL success = readFromPipe(readerPipe, dataPt, 2);
             if(success)
             {
-                if(success)
-                {
-                    std::cout << static_cast<int>(dataPt[0]) << " " << static_cast<int>(dataPt[1]) << std::endl;
-                }
-                // BOOL success = writeToPipe(writerPipe, &dataPt, 2);
+                BOOL success = writeToPipe(writerPipe, dataPt, 2);
             }
         }
     }
@@ -101,11 +75,10 @@ int main(int argc, char *argv[])
     }
     std::srand(static_cast<unsigned int>(std::time(0)));
     // starting the thread for receiving the data
-    std::thread t3(sendDataToFilterBlock);
+    std::thread t2(sendDataToFilterBlock);
     std::this_thread::sleep_for(std::chrono::milliseconds(1)); // wait for pipe to be create
     std::thread t1(randomNumberGen1); // then start client
 
-    t3.join();
     t2.join();
     t1.join();
 
