81ef6f131be53b269b1d5630976d2948b2f61122
91932 <krishnapal.jadeja@learner.manipal.edu>
2025-07-10 19:53:29 +0530

    minor change to line cam code

--- lineCamera.cpp
+++ lineCamera.cpp
@@ -5,16 +5,18 @@
 
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Invalid number of arguments" << std::endl;
-        return 1;
-    } else if (argc == 2) {
+    if (argc == 2) {
         launchProcess("bin\\dataGenBlock.exe");
         launchProcess("bin\\filterAndThresholdBlock.exe", argv[1]);
     } else if (argc == 4) {
         std::string combinedArgs = std::string(argv[2]) + " " + argv[3];
         launchProcess("bin\\dataGenBlock.exe", combinedArgs);
         launchProcess("bin\\filterAndThresholdBlock.exe", argv[1]);
+    }
+    else
+    {
+        std::cerr << "Invalid number of args " << std::endl;
+        return 1;
     }
 
     return 0;
